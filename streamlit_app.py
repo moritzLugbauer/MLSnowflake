5ccba0a5f4c5165644c736fb54788ff68c5ed601
moritzLugbauer <97183370+moritzLugbauer@users.noreply.github.com>
2024-09-14 14:32:21 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -44,3 +44,9 @@ if time_to_insert:
     st.success('Your Smoothie is ordered, ' + name_on_order + '!', icon="✅")
 
 
+# New section to display fruityvice nutrition information
+import requests
+fruityvice_response = requests.get ("https://fruityvice.com/api/fruit/watermelon")
+st. text (fruityvice_response)
+
+
